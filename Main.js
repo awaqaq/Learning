663a7d1ae3d7720e2e45660fd01a7915a30ba8a6
awaqaq <98203268+awaqaq@users.noreply.github.com>
2024-05-20 20:17:46 +0800

    1

--- Main.js
+++ Main.js
@@ -1,1 +1,78 @@
+const message = 'Hello world' // Try edit me
+const size = 4;
 
+// Update header text
+//document.querySelector('#header').innerHTML = message;
+
+var board = [
+  [2,2,8,4],
+  [2,0,4,0],
+  [0,0,4,8],
+  [2,4,0,0]
+]
+
+board = DropDown(board);
+board = DropUp(board);
+console.log(board);
+Show()
+
+function Update()
+{
+    //board = Add(board);
+    Show();
+}
+function Show(){
+    document.querySelector('#board').innerHTML= board.map((value)=>value.toString()).join("<br>");
+}
+function Add(board){
+    var index = Math.ceil(Math.random()*(size*size));
+    for(i =0;;i = (i+1)%size)){
+        for(j=0;;(j+1)%size){
+            if(board[i][j] == 0) {
+                if(index < 0) {
+                    board[i][j] =2;
+                    return board;
+                }
+                else indes--;
+            }
+        }
+    }
+}
+
+function DropLeft(board){
+  board.forEach(LineDrop);
+  return board;
+
+  function LineDrop(line){
+    for(i = 1;i < size;i++){
+      for(j = i;j >= 1;j--){
+        if(line[j-1] == 0){
+           line[j-1] = line[j];
+           line[j] = 0;
+        }
+        else if(line[j-1] == line[j]) {
+          line[j-1] *= 2;
+          line[j] = 0;
+        }
+      }
+    }
+  }
+}
+function DropRight(board){
+  board.forEach((line)=>line.reverse());
+  board = DropLeft(board);
+  board.forEach((line)=>line.reverse());
+  return board;
+}
+function DropDown(board){
+  board = board.map((row, index) => board.map(col => col[index]).reverse());
+  board = DropLeft(board);
+  board = board.map((row, index) => board.map(col => col[size - 1 - index]));
+  return board;
+}
+function DropUp(board){
+  board = board.map((row, index) => board.map(col => col[board.length - 1 - index]));
+  board = DropLeft(board);
+  board = board.map((row, index) => board.map(col => col[index]).reverse());
+  return board;
+}
